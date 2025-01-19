a43fea950718e1329c82ed29b80cc1fe54f1fed6
Joe <joseph5062a@gmail.com>
2025-01-18 18:55:54 -0800

    blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import { Lato } from "next/font/google";
 import blogRecents from '../data/blogRecents';
+import recipeData from '../data/recipeData';
+import BlogBox from "../components/BlogBox";
+import Link from "next/link";
 
 const header = localFont({
     src: "../fonts/NicoMoji.ttf",
@@ -32,32 +35,69 @@ const Page = () => {
         >
           <Grain opacity={.95} blendMode="multiply" />
           <div className="w-full flex justify-center items-center min-h-[100vh]">
-            <DiagonalCornerContainer className="w-[60rem] mx-2" size={48} bottomRightStraight>
+            <DiagonalCornerContainer className="m-2 min-[925px]:hidden w-full max-w-[450px] text-white" size={64}>
+              <div className="min-[425px]:text-[4.7rem] text-[3.5rem] leading-[3rem] mt-4 w-full flex justify-center">
+                <h1 className={header.className}>
+                  Blog
+                </h1>
+              </div>
+              <div className="mt-12 space-y-2">
+                {blogRecents.map(blog => (
+                  <BlogBox mobile full_title={blog.full_title} link={blog.route} tag_line={blog.tag_line} key={blog.title} />
+                ))}
+              </div>
+              <div className="min-[425px]:text-[4.7rem] text-[3.5rem] leading-[3rem] mt-12 w-full flex justify-center">
+                <h1 className={header.className}>
+                  Recipes
+                </h1>
+              </div>
+              <div className="mt-12 mb-16 grid grid-cols-2 gap-x-2">
+                {recipeData.map(recipe => (
+                  <Link className="hover:text-teal-300 flex justify-center" href={recipe.route} key={recipe.name}>
+                    <div className="flex space-x-4 text-sm">
+                      {/* <p>&#8226;</p> */}
+                      <p className={text.className}>{recipe.name}</p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </DiagonalCornerContainer>
+            <DiagonalCornerContainer className="w-[58rem] mx-2 min-[925px]:flex hidden" size={48} bottomRightStraight>
               <div className="w-full flex justify-between">
-                <div className="text-white w-[33.75rem] mt-4">
-                  <div className="text-[4.5rem] leading-[3rem] ml-4">
+                <div className="text-white min-w-[34rem] mt-4 mr-8">
+                  <div className="text-[4.7rem] leading-[3rem] ml-4">
                     <h1 className={header.className}>
                       Blog Posts
                     </h1>
                   </div>
-                  <div className="mt-14 space-y-3">
+                  <div className="mt-9 space-y-2 h-[23.5rem] overflow-y-scroll pr-2">
                     {blogRecents.map(blog => (
-                      <div className="text-xl flex space-x-3">
-                        <p>&#8226;</p>
-                        <p className={text.className}>
-                          {blog.full_title}
-                        </p>
-                      </div>
+                      <BlogBox full_title={blog.full_title} link={blog.route} tag_line={blog.tag_line} key={blog.title} />
+                    ))}
+                  </div>
+                  <div className="text-[4.7rem] leading-[3rem] ml-4 mt-8">
+                    <h1 className={header.className}>
+                      Recipes
+                    </h1>
+                  </div>
+                  <div className="mt-9 mb-6 grid grid-cols-2">
+                    {recipeData.map(recipe => (
+                      <Link className="hover:text-teal-300" href={recipe.route} key={recipe.name}>
+                        <div className="flex space-x-4">
+                          <p>&#8226;</p>
+                          <p className={text.className}>{recipe.name}</p>
+                        </div>
+                      </Link>
                     ))}
                   </div>
                 </div>
-                <div className="flex flex-col justify-center">
-                <div className="h-[3.75rem] relative mb-2 mt-4">
-                    <p className="text-white text-[.65rem] italic absolute max-w-[19rem]">
-                        {"Without words, without writing and without books there would be no history, there could be no concept of humanity."}
+                <div className="flex-col justify-center flex">
+                <div className="h-[3.75rem] relative mb-[.95rem] mt-[1.65rem]">
+                    <p className="text-white text-[.7rem] italic absolute max-w-[19rem]">
+                        {"In the very books in which philosophers bid us scorn fame, they inscribe their names."}
                     </p>
-                    <div className="flex justify-end text-white text-[.65rem] bottom-3 right-0 absolute">
-                    {"― Hermann Hesse, 1904"} 
+                    <div className="flex justify-end text-white text-[.7rem] bottom-2 right-0 absolute">
+                    {"― Marcus Cicero, 102 BC"} 
                     </div>
                 </div>
                   <Image className="border-white border" src={"/img/libg.jpg"} width={320} height={600} alt="Library"/>
---
+++ src/app/components/BlogBox.js
@@ -0,0 +1,69 @@
+import Grain from "./Grain";
+
+const BlogBox = ({ 
+  full_title,
+  tag_line,
+  link,
+  className = '',
+  size = 24,
+  mobile = false
+}) => {
+  const outerClipPath = `polygon(
+    0 0,
+    calc(100% - ${size}px) 0,
+    100% ${size}px,
+    100% 100%,
+    0 100%
+  )`;
+
+  const innerClipPath = `polygon(
+    1px 1px,
+    calc(100% - ${size + 1}px) 1px,
+    calc(100% - 1px) ${size + 1}px,
+    calc(100% - 1px) calc(100% - 1px),
+    1px calc(100% - 1px)
+  )`;
+
+  return (
+    <div className={`relative ${className}`}>
+      {/* Border container */}
+      <div
+        className="absolute inset-0 bg-white"
+        style={{
+          clipPath: outerClipPath,
+        }}
+      >
+        {/* Inner dark container */}
+        <div 
+          className="absolute inset-0 overflow-hidden"
+          style={{
+            clipPath: innerClipPath,
+            backgroundColor: 'rgb(31, 31, 31)'
+          }}
+        />
+      </div>
+      
+      <a href={link ? link : "/"} target="_blank" referrerPolicy="noopener noreferrer">
+        <div
+          className="relative z-10 hover:bg-teal-700 hover:cursor-ne-resize duration-75 transition-colors p-2"
+          style={{ clipPath: outerClipPath }}
+        >
+          <div className="space-y-0.5">
+            <div className={`${mobile ? "mr-4 text-[.95rem]" : "text-xl"} font-bold`}>
+              <p className="text-white">
+                {full_title}
+              </p>
+            </div>
+            <div className={`${mobile ? "font-thin" : ""} text-[.7rem] text-teal-200`}>
+              <p>
+                {tag_line}
+              </p>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default BlogBox;
