e3ef78ebb2a041bf7c76e464c6b9ff2010bdf762
Joe <joseph5062a@gmail.com>
2025-01-19 00:53:11 -0800

    pics start

---
+++ src/app/components/CustomLightBox.js
@@ -0,0 +1,52 @@
+import Lightbox from "react-spring-lightbox";
+
+const CustomLightBox = ({
+    images,
+    currentImageIndex,
+    setCurrentIndex,
+    isOpen,
+    onClose
+  }) => {
+
+    const gotoPrevious = () =>
+      currentImageIndex > 0 && setCurrentIndex(currentImageIndex - 1);
+  
+    const gotoNext = () =>
+      currentImageIndex + 1 < images.length &&
+      setCurrentIndex(currentImageIndex + 1);
+  
+    return (
+      <Lightbox
+        isOpen={isOpen}
+        onPrev={gotoPrevious}
+        onNext={gotoNext}
+        images={images}
+        currentIndex={currentImageIndex}
+        style={{ background: "rgba(0, 0, 0, .7)" }}
+        onClose={onClose}
+        singleClickToZoom
+        renderImageOverlay={() => {
+          return (
+            <div
+              style={{
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                left: 0,
+                display: "flex",
+                justifyContent: "center",
+                color: "#379683",
+                fontSize: "1.5em",
+                padding: 3,
+                fontFamily: "Times New Roman, Times, serif",
+              }}
+            >
+              {images[currentImageIndex].title}
+            </div>
+          );
+        }}
+      />
+    )
+};
+
+export default CustomLightBox;
---
+++ src/app/gallery/la/page.js
@@ -0,0 +1,41 @@
+import Grain from "../../components/Grain"
+import DiagonalCornerContainer from "../../components/DiagonalCornerContainer";
+import Image from "next/image";
+import { Lato } from "next/font/google";
+
+
+const text = Lato({
+  subsets: ['latin'],
+  weight: ["100", "400"]
+});
+
+const Page = () => {
+    return (
+      <div className="bg-black">
+        <div
+          className="w-full min-h-screen relative"
+          style={{
+            background: `linear-gradient(135deg,
+              rgb(31, 31, 31) 0%,
+              rgb(57, 173, 161, .5) 50%,
+              rgb(31, 31, 31) 100%)`
+          }}
+        >
+          <Grain opacity={.95} blendMode="multiply" />
+          <div className="w-full min-h-[100vh] flex justify-center p-2 py-6">
+            <DiagonalCornerContainer className="w-[60rem]" size={64}>
+                <div className={text.className}>
+                    <div className='mx-auto pt-8 md:px-20'>
+                        <div className='text-white text-left mx-5 mb-8'>
+                            
+                        </div>
+                    </div>
+                </div>
+            </DiagonalCornerContainer>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Page;
