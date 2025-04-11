71750bf622bd23d12cd9efde5f550f355877b487
Joseph Weller <joseph5062a@gmail.com>
2025-04-10 21:24:02 -0700

    minor updates

--- src/app/blog/pizza/page.js
+++ src/app/blog/pizza/page.js
@@ -45,10 +45,10 @@ const Page = () => {
                             {"// "}if you haven&apos;t happened to make a pizza from scratch before, I highly recommend watching {styledLink("this video", "https://www.youtube.com/watch?v=SDpCzJw2xm4")} first
                           </div>
                           {/* <div className='md:text-xl mt-5'>
-                            <Link href='/bucketlist' target="_blank" rel="noreferrer"><span className='inline underline hover:text-teal hover:cursor-pointer'>Bucket List</span></Link> Progress as of 1/10/25: <span className='text-navy font-bold'>141/1000</span>
+                            <Link href='/bucketlist' target="_blank" rel="noreferrer"><span className='inline underline hover:text-teal hover:cursor-pointer'>Bucket List</span></Link> Progress as of 1/10/25: <span className='text-navy font-bold'>153/1000</span>
                           </div>
                           <div className="w-full bg-mint rounded-full h-2 mt-2">
-                            <div className="bg-navy h-2 rounded-full" style={{"width": ((141/1000.0)*100) + "%"}}></div>
+                            <div className="bg-navy h-2 rounded-full" style={{"width": ((153/1000.0)*100) + "%"}}></div>
                           </div> */}
                           <div className='md:text-xl space-y-5 mt-8'>
                             <div className='align-text-bottom'>
---
+++ src/app/gallery/ce/page.js
@@ -0,0 +1,64 @@
+"use client";
+import Grain from "../../components/Grain"
+import DiagonalCornerContainer from "../../components/DiagonalCornerContainer";
+import { Lato } from "next/font/google";
+import { useState } from "react";
+import CustomLightBox from "@/app/components/CustomLightBox";
+import ImageMosaic from "@/app/components/ImageMosaic";
+
+const text = Lato({
+  subsets: ['latin'],
+  weight: ["100", "400"]
+});
+
+const Page = () => {
+
+    const images = [
+        {
+            src: '/img/italy_final/Italy-01.jpg',
+        },
+      ];
+
+    const [isOpen, setOpen] = useState(false);
+    const [currentImageIndex, setCurrentIndex] = useState(0);
+
+    return (
+      <div className="bg-black">
+        <div
+          className="w-full min-h-screen relative"
+          style={{
+            background: `linear-gradient(135deg,
+              rgb(31, 31, 31) 0%,
+              rgb(57, 173, 161, .5) 50%,
+              rgb(31, 31, 31) 100%)`
+          }}
+        >
+          <Grain opacity={.95} blendMode="multiply" />
+          <div className="w-full min-h-[100vh] flex justify-center p-2 py-6">
+            <DiagonalCornerContainer className="w-[60rem]" size={32}>
+                <div className={text.className}>
+                    <div className='mx-auto pt-8 md:px-20'>
+                        <div className='text-white text-left sm:mx-5 mb-4 sm:mb-24'>
+                            <div className='text-4xl font-bold'>
+                                Central Europe
+                            </div>
+                            <div className='text-teal-500 text-xl space-y-5 my-8'>
+                                <p>{"// Shot on Fujifilm X-T30ii, 27mm w/ Reggie's Portra"}</p>
+                            </div>
+                            <ImageMosaic images={images}
+                                handleClick={({ index }) => {
+                                setCurrentIndex(index);
+                                setOpen(true);
+                            }}/>
+                            <CustomLightBox images={images} currentImageIndex={currentImageIndex} setCurrentIndex={setCurrentIndex} isOpen={isOpen} onClose={() => setOpen(false)}/>
+                        </div>
+                    </div>
+                </div>
+            </DiagonalCornerContainer>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Page;
